13f433cc894afe08763a62f831fb67c7467f7575
lincolnzera1 <88245209+lincolnzera1@users.noreply.github.com>
2022-09-18 18:08:59 -0300

    Visual of the login page concluded

--- src/components/Login.js
+++ src/components/Login.js
@@ -8,18 +8,20 @@ import { FaLock } from 'react-icons/fa'
 const Login = () => {
   return (
     <div className='container'>
-        <div className="child">
-            <div className="inputs">
-                <HiOutlineMail size={40} color='white' className="icon-input"/>
-                <input type="text" placeholder='E-MAIL'/>
+        <form action="">
+            <div className="child">
+                <div className="inputs">
+                    <HiOutlineMail size={40} color='white' className="icon-input"/>
+                    <input type="text" placeholder='E-MAIL'/>
+                </div>
+                <div className="inputs">
+                    <input type="password" placeholder='SENHA'/>
+                    <FaLock size={35} color='white' className='icon-input'/>
+                </div>
+                <input type="submit" value='LOGIN'/>
+                <a href="#"><p>Esqueceu a senha?</p></a>
             </div>
-            <div className="inputs">
-                <input type="text" placeholder='SENHA'/>
-                <FaLock size={35} color='white' className='icon-input'/>
-            </div>
-            <input type="submit" value='LOGIN'/>
-            <a href="#"><p>Esqueceu a senha?</p></a>
-        </div>
+        </form>
     </div>
   )
 }
