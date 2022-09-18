226b07726f21a9b5966b3ad353f150c1f49ba3f5
lincolnzera1 <88245209+lincolnzera1@users.noreply.github.com>
2022-09-18 20:13:30 -0300

    Using react-router-dom, especially the useNavigate, that redirect to a other page if login is valid

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
 import './App.css';
 import Login from './components/Login';
-import Logo from './components/Logo';
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Home from './components/Home';
 
 function App() {
   return (
     <div className="App">
-      <Logo />
-      <Login />
+      
+      
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={ <Login /> }/>
+          {<Route path='/home' element={ <Home /> }/>}
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
--- src/components/Login.js
+++ src/components/Login.js
@@ -1,25 +1,44 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './Login.css'
 
 // Icons
 import { HiOutlineMail } from 'react-icons/hi'
 import { FaLock } from 'react-icons/fa'
+import Logo from './Logo'
+
+// Routes
+import { Link } from 'react-router-dom'
+import { useNavigate } from 'react-router-dom';
 
 const Login = () => {
+
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+    let navigate = useNavigate();
+
+    const useHandleSubmit = (e) => {
+        e.preventDefault()
+
+        navigate('/home')
+        
+
+    }
+
   return (
     <div className='container'>
-        <form action="">
+        <Logo />
+        <form action="" onSubmit={useHandleSubmit}>
             <div className="child">
                 <div className="inputs">
                     <HiOutlineMail size={40} color='white' className="icon-input"/>
-                    <input type="text" placeholder='E-MAIL'/>
+                    <input type="text" placeholder='E-mail' onChange={(e) => setEmail(e.target.value)}/>
                 </div>
                 <div className="inputs">
-                    <input type="password" placeholder='SENHA'/>
+                    <input type="password" placeholder='Senha' onChange={(e) => setPassword(e.target.value)}/>
                     <FaLock size={35} color='white' className='icon-input'/>
                 </div>
                 <input type="submit" value='LOGIN'/>
-                <a href="#"><p>Esqueceu a senha?</p></a>
+                <a href="##"><p>Esqueceu a senha?</p></a>
             </div>
         </form>
     </div>
