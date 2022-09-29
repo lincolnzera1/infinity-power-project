9f078715b7b110ac59321231e38f8174f58398c3
lincolnzera1 <88245209+lincolnzera1@users.noreply.github.com>
2022-09-29 17:19:59 -0300

    I think i love animation css props

--- src/components/Home.js
+++ src/components/Home.js
@@ -5,6 +5,7 @@ import Sidebar from './Sidebar'
 const Home = () => {
 
   const [controle, setControle] = useState(true)
+  const [isOn, setIsOn] =useState(true)
 
     const handleEvent = () => {
 
@@ -13,17 +14,19 @@ const Home = () => {
 
     }
 
+    const handleButtonEvent = () => {
+      setIsOn(!isOn)
+      console.log("Pressing this " +isOn )
+    }
+
   return (
     <div className='teste'>
       
-      <Sidebar /> 
-      <div className='quadrado'>Quarto 1</div>
-      <div className='quadrado'>Quarto 2</div>
-      <div className='quadrado'>Quarto 3</div>
-      <div className='quadrado'>Quarto 4</div>
-      <div className='quadrado'>Quarto 5</div>
-      <div className='quadrado'>Quarto 6</div>
+      <button onClick={handleButtonEvent} >Mudança</button>
 
+      {isOn === true ? (<div className="quadrado-online"></div>) 
+      : 
+      (<div className="quadrado-offline"></div>)}
 
         
 
