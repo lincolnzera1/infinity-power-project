d6bb71e23f5a7a9393c12507699ada369a89c191
lincolnzera1 <88245209+lincolnzera1@users.noreply.github.com>
2022-10-12 21:30:37 -0300

    Adicionei os nomes nos eixos dos graficos, e estou tentando adicionar o nome do mês no gráfico também

--- src/components/Home.js
+++ src/components/Home.js
@@ -37,32 +37,7 @@ const Home = () => {
       left: "50%",
       right: "auto",
       marginRight: '-50%',
-      transform: "translate(-50%, -50%)", 
-      [0, 0, 0],
-    [1, 10, 5],
-    [2, 23, 15],
-    [3, 17, 9],
-    [4, 18, 10],
-    [5, 9, 5],
-    [6, 11, 3],
-    [7, 27, 19],
-    [8, 0, 0],
-    [9, 0, 0],
-    [10, 0, 0],
-    [11, 0, 0],
-    [12, 0, 0],
-    [13, 0, 0],
-    [14, 0, 0],
-    [15, 0, 0],
-    [16, 0, 0],
-    [17, 0, 0],
-    [18, 0, 0],
-    [19, 0, 0],
-    [20, 0, 0],
-    [21, 0, 0],
-    [22, 0, 0],
-    [23, 0, 0],
-    [24, 11, 5],
+      transform: "translate(-50%, -50%)"
       
       */
     }
@@ -79,7 +54,14 @@ const Home = () => {
   ];
   
   const pieOptions = {
-    curveType: "function"
+    title: "here",
+    curveType: "function",
+    hAxis: {
+      title: "dia do mês",
+    },
+    vAxis: {
+      title: "Potência em kWh",
+    },
   };
 
 
@@ -158,6 +140,8 @@ const Home = () => {
         console.log("Everything is ok")
         //console.log(Object.values(response.data))
         setChartData(loadData(response.data))
+
+        console.log((response.data)[0])
         /* for(var i in response.data){
           
           setStatus("Offline")
